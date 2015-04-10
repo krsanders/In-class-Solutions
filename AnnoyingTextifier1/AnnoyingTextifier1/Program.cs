8af98841a0db9818355aace3a474bc706129af29
Kris Sanders <sandersksw@gmail.com>
2015-04-09 18:28:48 -0600

    Started GroupPicker program for pgm assignment#2

---
+++ AnnoyingTextifier1/AnnoyingTextifier1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnoyingTextifier1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AnNoYiNgTeXtIfYeR("gobbledygook"));
+            Console.ReadKey();
+        }
+            static string AnNoYiNgTeXtIfYeR(string notAnnoyingString)
+        {
+            //input: "hello"
+            //output: "hELlO"
+            //declare a return string for output
+            string returnString = string.Empty;
+
+            for (int i = 0; i < notAnnoyingString.Length; i++)
+            {
+                //get a letter to examine
+                string letter = notAnnoyingString[i].ToString();
+
+
+                //looping over every letter
+                //get a letter to examine
+                //see if the position of the letter is odd or even
+
+                if (i % 2 == 0)
+                {
+                    //if even (or odd, whatever the preference is) make it CaPiTal)
+                    returnString = returnString + letter.ToUpper();
+                }
+                else
+                {
+                    returnString += letter.ToLower();
+                }
+            }
+            return returnString;
+         //   for (int i = 0; i < notAnnoyingString.Length; i++)
+         //   {
+                
+         //   }
+            
+        
+        }
+    }
+}
---
+++ NumberRounder1/NumberRounder1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberRounder1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("The nearest round number is: " + NumberRound(41));
+        Console.ReadKey();
+        }
+        // 1 % 5 = 1. Round down
+        // 2 % 5 = 2  Round down
+        // 3 % 5 = 3  Round up
+        // 4 % 5 = 4  Round up
+        // 5 % 5 = 0  NO rounding in either direction
+        // 6 % 5 = 1  Round down
+        // 7 % 5 = 2  Round down
+        //...for 8 and 9 until 10 and etc.
+
+        static int NumberRound(int numberToRound)
+        
+        int remainder = numberToRound % 5;
+
+        if (remainder >= 3)
+        {
+        // Round UP
+        numberToRound = numberToRound += (5 - remainder);
+        return numberToRound;
+        }
+        else
+        {
+        numberToRound = numberToRound - remainder;
+        return numberToRound;
+        }
+        return 0;
+    
+    }
+}
